d6c30ca4b8f8575ec1d8f77b84d77310f808ad18
Lauren McCarthy <laurmccarthy@gmail.com>
2015-04-01 19:27:59 -0400

    engine

---
+++ Twilio/twilio_text_server/text_server.js
@@ -0,0 +1,47 @@
+// node-twilio docs: http://twilio.github.io/twilio-node/
+// TwiML xml response format docs: https://www.twilio.com/docs/api/twiml 
+
+// npm install twilio
+var twilio = require('twilio');
+
+// npm install express
+// setup express app
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+var server = require('http').Server(app);
+server.listen(app.get('port'));
+
+
+app.get('/', function(req, res) {
+  res.send('ok');
+});
+
+// set up routes
+app.get('/hello', function(req, res) {
+
+  var twiml = new twilio.TwimlResponse();
+  twiml.say('hello!');
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+app.get('/feeling', function(req, res) {
+  var digit = req.query.Digits;
+
+  var twiml = new twilio.TwimlResponse();
+  twiml.say('oh cool');
+  if (digit == '1') {
+    twiml.say('I am glad to hear that.');
+  } else if (digit == '2') {
+    twiml.say('I am sorry to hear that.');
+  } else {
+    twiml.say('Say what?');
+  }
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+
+});
+
